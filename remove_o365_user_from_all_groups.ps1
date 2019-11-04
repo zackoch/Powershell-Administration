bcd7c33d2c21147f883f2deca67846af4a301907
Zac Koch <zackoch@gmail.com>
2019-11-04 15:13:58 -0600

    init

---
+++ remove_o365_user_from_all_groups.ps1
@@ -0,0 +1,21 @@
+$globaladmin = "first.last@example.com"
+$credentials = Get-Credential -Credential $globaladmin
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $credentials -Authentication Basic -AllowRedirection
+
+Import-PSSession $Session -DisableNameChecking
+
+$user_email = read-host "What's the users email address?"
+$mail = get-mailbox $user_email
+
+$all_groups = Get-DistributionGroup | select *
+
+foreach($group in $all_groups){
+    $group_members = get-distributionGroupMember -identity $group.Id
+    foreach($member in $group_members){
+        if($member.name -eq $mail.name){
+            write-host "Removing" $mail.name "from group" $group.Name"..."
+            remove-distributiongroupmember $group.Name -Member $user_email -confirm:$false
+            write-host "done..."
+        }
+    }
+}
