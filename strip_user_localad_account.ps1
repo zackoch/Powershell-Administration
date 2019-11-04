d381492e19551a2306aadcf3c18b484e462404d0
Zac Koch <zackoch@gmail.com>
2019-11-04 12:40:12 -0600

    init

---
+++ strip_user_localad_account.ps1
@@ -0,0 +1,9 @@
+$ADsession = New-PSSession -ComputerName dc.yourdomaincontroller.com
+Import-Module -PSSession $ADsession ActiveDirectory
+
+$user_email = Read-Host -Prompt "What's the users email address?"
+$user_object = Get-ADUser -LDAPFilter "(userPrincipalName=$($user_email))" -Properties *
+
+write-host "Clearing AD attributes..."
+set-ADObject -Identity $user_object.DistinguishedName -clear Department, Description, DisplayName, extensionAttribute1, mail, Manager, physicalDeliveryOfficeName, telephoneNumber, Title, Mobile, Company
+write-host "Done!"
