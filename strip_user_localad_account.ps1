6eafd67898caa6a6ee6598b042e8ce8a80d493fa
Zac Koch <zackoch@gmail.com>
2019-12-19 09:17:38 -0600

    added home phone, city, zip, street, and state

--- strip_user_localad_account.ps1
+++ strip_user_localad_account.ps1
@@ -5,5 +5,5 @@ $user_email = Read-Host -Prompt "What's the users email address?"
 $user_object = Get-ADUser -LDAPFilter "(userPrincipalName=$($user_email))" -Properties *
 
 write-host "Clearing AD attributes..."
-set-ADObject -Identity $user_object.DistinguishedName -clear Department, Description, DisplayName, extensionAttribute1, mail, Manager, physicalDeliveryOfficeName, telephoneNumber, Title, Mobile, Company
+set-ADObject -Identity $user_object.DistinguishedName -clear Department, Description, DisplayName, extensionAttribute1, mail, Manager, physicalDeliveryOfficeName, telephoneNumber, Title, Mobile, Company, homePhone, l, postalCode, streetAddress, st
 write-host "Done!"
